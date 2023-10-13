d87afab1d15045ea9199512465d3a4ab9e9bd761
satishvatturu <93177666+satishvatturu@users.noreply.github.com>
2023-10-13 16:53:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 //your code here
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+
+function removeArticles(name){
+    return name.replace(/^(the|a|an)\s+/i, "");
+}
+
+touristSpots.sort((a,b)=> {
+    if(removeArticles(a)>removeArticles(b))
+    {
+        return 1;
+    }
+    if(removeArticles(a)<removeArticles(b))
+    {
+        return -1;
+    }
+    return 0;
+});
+
+let ulElement = document.getElementById('band');
+
+for(let spots of touristSpots)
+{
+    let liElement = document.createElement('li');
+    liElement.textContent=spots;
+    ulElement.appendChild(liElement);
+}
+
 
